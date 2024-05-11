4414d923660d84084eca0e2141bde85db3245f6f
RmataM7 <rmuadthong@gmail.com>
2024-05-11 00:01:26 -0700

    add metavar in parse argument

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description='It does frame IO... stuff..')
 
-parser.add_argument("--baselight", help="Baselight stuff")
-parser.add_argument("--xytech", help="Xytech stuff")
-parser.add_argument("--process", help="Process stuff")
-parser.add_argument("--output", help="Output stuff")
+parser.add_argument("--baselight", metavar='', help="Baselight stuff")
+parser.add_argument("--xytech", metavar='', help="Xytech stuff")
+parser.add_argument("--process", metavar='', help="Process stuff")
+parser.add_argument("--output", metavar='', help="Output stuff")
 # debug, argparse
-parser.add_argument("--frame-timecode", type=int, help="Frame to timecode")
+parser.add_argument("--frame-timecode", metavar='', type=int, help="Frame to timecode; must follow by an integer to calcualte")
 
 args = parser.parse_args()
 
-# convert frame to timestamp
+# convert frame to time code
 def frame_to_timecode(frame, fps=24):
     hours = frame // (3600 * fps)
     frame %= 3600 * fps
@@ -22,5 +22,4 @@ def frame_to_timecode(frame, fps=24):
     return "{:02d}:{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds, frame)
 
 if args.frame_timecode:
-    # print(frame_to_timecode(args.frame_timecode))
     print(f"Frame {args.frame_timecode} is {frame_to_timecode(args.frame_timecode)} at 24 fps")
